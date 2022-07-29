2b5d71f5139fc8e4e7c989cbab88db39d0df4c2a
ALITTLECHILD <XIAO20030304@163.com>
2022-07-29 20:54:03 +0800

    GitPractice Seventh Upload!!!

--- src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
+++ src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
@@ -13,6 +13,11 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 
 }
