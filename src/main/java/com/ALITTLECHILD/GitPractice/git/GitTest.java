5af019754b237a80116e88f540cf67a19e8b6c4c
ALITTLECHILD <XIAO20030304@163.com>
2022-07-29 20:34:19 +0800

    GitPractice First Upload!!!

---
+++ src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
@@ -0,0 +1,18 @@
+/**
+ * @description : [一句话描述该类的功能]
+ * @author : [小孩子嘛！]
+ * @version : [v1.0]
+ * @createTime : [2022/7/29 20:28]
+ * @updateUser : [小孩子嘛！]
+ * @updateTime : [2022/7/29 20:28]
+ * @updateRemark : [说明本次修改内容]
+ */
+package com.ALITTLECHILD.GitPractice.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
