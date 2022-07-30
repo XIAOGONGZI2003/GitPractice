e3aebc9677798fb513edd58898659987b04a5e67
ALITTLECHILD <87901211+XIAOGONGZI2003@users.noreply.github.com>
2022-07-30 10:18:47 +0800

    Update GitTest.java
    我在GitHub网站上的一次修改！

--- src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
+++ src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
@@ -19,6 +19,9 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("push test!");
+        System.out.println("push test2!");
+        System.out.println("push test3!");
+        System.out.println("push test4!");
     }
 
 }
