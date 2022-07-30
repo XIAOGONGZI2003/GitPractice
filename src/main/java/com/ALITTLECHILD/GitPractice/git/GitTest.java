5a84f3d0593503b00641c9526e0bbc1f6aea8906
ALITTLECHILD <XIAO20030304@163.com>
2022-07-30 10:04:02 +0800

    GitPractice Upload-Push Test!!!

--- src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
+++ src/main/java/com/ALITTLECHILD/GitPractice/git/GitTest.java
@@ -18,6 +18,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("push test!");
     }
 
 }
